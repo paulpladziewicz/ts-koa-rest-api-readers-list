756c72d2264fba637ce7ec9614fc30d33c05a514
paulpladziewicz <ppladziewicz@gmail.com>
2022-05-29 20:30:40 -0400

    ran prettier

--- src/routes/apiRouter.ts
+++ src/routes/apiRouter.ts
@@ -1,18 +1,18 @@
-import Router from 'koa-router'
+import Router from 'koa-router';
 
-import authRouter from './authentication'
-import nytimesRouter from './nytimes'
+import authRouter from './authentication';
+import nytimesRouter from './nytimes';
 
 // not sure if { prefix: '/api' } option will be needed yet
 const apiRouter = new Router({ prefix: '/api' });
 
 apiRouter.get('/', async (ctx) => {
-    ctx.body = {message: 'Readers List API is healthy.'};
+  ctx.body = { message: 'Readers List API is healthy.' };
 });
 
-const nestedRoutes = [authRouter, nytimesRouter]
+const nestedRoutes = [authRouter, nytimesRouter];
 for (let router of nestedRoutes) {
-    apiRouter.use(router.routes(), router.allowedMethods())
+  apiRouter.use(router.routes(), router.allowedMethods());
 }
 
 export default apiRouter;
--- src/routes/nytimes.ts
+++ src/routes/nytimes.ts
@@ -1,13 +1,13 @@
 import Router from 'koa-router';
-import axios from "axios";
+import axios from 'axios';
 
 const nytimesRouter = new Router();
 
 nytimesRouter.get('/nytimes-bestsellers', async (ctx) => {
-    const { data } = await axios.get(
-        `https://api.nytimes.com/svc/books/v3/lists/combined-print-and-e-book-nonfiction.json?api-key=${process.env.NYTIMES_API_KEY}`
-    );
-    ctx.body = data;
+  const { data } = await axios.get(
+    `https://api.nytimes.com/svc/books/v3/lists/combined-print-and-e-book-nonfiction.json?api-key=${process.env.NYTIMES_API_KEY}`
+  );
+  ctx.body = data;
 });
 
 export default nytimesRouter;
